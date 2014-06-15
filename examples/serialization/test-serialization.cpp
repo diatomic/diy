1a6bebdc6f8d08563f1a3743b4feed07a38fbd1f
Dmitriy Morozov <dmitriy@mrzv.org>
2014-06-15 13:01:43 -0700

    Added elementary serialization

---
+++ examples/serialization/test-serialization.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <diy/serialization.hpp>
+
+// Point does not need any special handling for serialization; it will be saved
+// as plain binary data
+struct Point
+{
+  int&  operator[](int i)       { return coords[i]; }
+  int   coords[3];
+};
+
+struct PointVec
+{
+        PointVec()              {}          // need this for de-serialization
+        PointVec(int dim):
+          coords(dim)           {}
+  int&  operator[](int i)       { return coords[i]; }
+  std::vector<int>  coords;
+};
+
+namespace diy
+{
+  template<>
+  struct Serialization<PointVec>
+  {
+    static void save(BinaryBuffer& bb, const PointVec& p)       { diy::save(bb, p.coords); }
+    static void load(BinaryBuffer& bb, PointVec& p)             { diy::load(bb, p.coords); }
+  };
+}
+
+int main()
+{
+  diy::BinaryBuffer bb;
+  std::cout << "Position: " << bb.position << std::endl;
+
+  {
+    std::vector<Point>    points;
+    Point p; p[0] = p[1] = p[2] = 5;
+    points.push_back(p);
+    p[0] = 1;
+    points.push_back(p);
+    p[2] = 1;
+    points.push_back(p);
+
+    save(bb, points);
+  }
+  std::cout << "Position: " << bb.position << std::endl;
+
+  {
+    std::vector<PointVec> points;
+    PointVec p(3);
+    points.push_back(p);
+    p[0] = 2;
+    points.push_back(p);
+    p[2] = 2;
+    points.push_back(p);
+
+    save(bb, points);
+  }
+  std::cout << "Position: " << bb.position << std::endl;
+
+  bb.reset();
+  std::cout << "Position: " << bb.position << std::endl;
+
+  {
+    std::vector<Point>    points;
+    load(bb, points);
+    for (unsigned i = 0; i < points.size(); ++i)
+    {
+      for (unsigned j = 0; j < 3; ++j)
+        std::cout << points[i][j] << ' ';
+      std::cout << std::endl;
+    }
+  }
+  std::cout << "Position: " << bb.position << std::endl;
+
+  {
+    std::vector<PointVec> points;
+    load(bb, points);
+    for (unsigned i = 0; i < points.size(); ++i)
+    {
+      for (unsigned j = 0; j < 3; ++j)
+        std::cout << points[i][j] << ' ';
+      std::cout << std::endl;
+    }
+  }
+  std::cout << "Position: " << bb.position << std::endl;
+}
---
+++ include/diy/serialization.hpp
@@ -0,0 +1,126 @@
+#ifndef DIY_SERIALIZATION_HPP
+#define DIY_SERIALIZATION_HPP
+
+#include <vector>
+#include <map>
+
+namespace diy
+{
+  struct BinaryBuffer
+  {
+                        BinaryBuffer(int position_ = 0):
+                          position(position_)                       {}
+
+    inline void         save_binary(const char* x, int count);
+    inline void         load_binary(char* x, int count);
+
+    void                reset()                                     { position = 0; }
+
+    int position;
+    std::vector<char>   buffer;
+  };
+
+  template<class T>
+  struct Serialization
+  {
+    static void         save(BinaryBuffer& bb, const T& x)          { bb.save_binary((const char*)  &x, sizeof(T)); }
+    static void         load(BinaryBuffer& bb, T& x)                { bb.load_binary((char*)        &x, sizeof(T)); }
+  };
+
+  // Specialize these functions for types that need special handling
+  template<class T>
+  void                  save(BinaryBuffer& bb, const T& x)          { Serialization<T>::save(bb, x); }
+
+  template<class T>
+  void                  load(BinaryBuffer& bb, T& x)                { Serialization<T>::load(bb, x); }
+
+  // save/load for std::vector<U>
+  template<class U>
+  struct Serialization< std::vector<U> >
+  {
+    typedef             std::vector<U>          Vector;
+
+    static void         save(BinaryBuffer& bb, const Vector& v)
+    {
+      unsigned s = v.size();
+      diy::save(bb, s);
+      for (unsigned i = 0; i < s; ++i)
+        diy::save(bb, v[i]);
+    }
+
+    static void         load(BinaryBuffer& bb, Vector& v)
+    {
+      unsigned s;
+      diy::load(bb, s);
+      v.resize(s);
+      for (unsigned i = 0; i < s; ++i)
+        diy::load(bb, v[i]);
+    }
+  };
+
+  // save/load for std::pair<X,Y>
+  template<class X, class Y>
+  struct Serialization< std::pair<X,Y> >
+  {
+    typedef             std::pair<X,Y>          Pair;
+
+    static void         save(BinaryBuffer& bb, const Pair& p)
+    {
+      diy::save(bb, p.first);
+      diy::save(bb, p.second);
+    }
+
+    static void         load(BinaryBuffer& bb, Pair& p)
+    {
+      diy::load(bb, p.first);
+      diy::load(bb, p.second);
+    }
+  };
+
+  // save/load for std::map<K,V>
+  template<class K, class V>
+  struct Serialization< std::map<K,V> >
+  {
+    typedef             std::map<K,V>           Map;
+
+    static void         save(BinaryBuffer& bb, const Map& m)
+    {
+      unsigned s = m.size();
+      diy::save(bb, s);
+      for (typename std::map<K,V>::const_iterator it = m.begin(); it != m.end(); ++it)
+        diy::save(bb, *it);
+    }
+
+    static void         load(BinaryBuffer& bb, Map& m)
+    {
+      unsigned s;
+      diy::load(bb, s);
+      std::pair<K,V> p;
+      for (unsigned i = 0; i < s; ++i)
+      {
+        diy::load(bb, p);
+        m.insert(p);
+      }
+    }
+  };
+}
+
+void
+diy::BinaryBuffer::
+save_binary(const char* x, int count)
+{
+  if (position + count > buffer.size())
+    buffer.resize(position + count);        // TODO: double-check that this does the right thing with capacity
+  std::copy(x, x + count, &buffer[position]);
+  position += count;
+}
+
+void
+diy::BinaryBuffer::
+load_binary(char* x, int count)
+{
+  std::copy(&buffer[position], &buffer[position + count], x);
+  position += count;
+}
+
+#endif
