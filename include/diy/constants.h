92f94c3cc253b4c6ff450361e1cfde4be33daf69
Dmitriy Morozov <dmitriy@mrzv.org>
2014-06-21 07:31:57 -0700

    Allow DIY_MAX_DIM to be specified by the user

--- include/diy/constants.h
+++ include/diy/constants.h
@@ -1,8 +1,11 @@
 #ifndef DIY_CONSTANTS_H
 #define DIY_CONSTANTS_H
 
-// TODO: accept this as a compile-time definition instead?
+// Default DIY_MAX_DIM to 4, unless provided by the user
+// (used for static array size in various Bounds (bb_t in types.h))
+#ifndef DIY_MAX_DIM
 #define DIY_MAX_DIM 4
+#endif
 
 /* DIY directions: neighbor direction enumeration
    used to identify direction of one neighbor block for both regular and 
