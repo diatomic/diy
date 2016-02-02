fa4d6faa22bff9fba7871c8cc061ac60d2000c19
Tom Peterka <tpeterka@mcs.anl.gov>
2016-02-01 19:53:33 -0600

    doxygen

--- search/functions_0.js
+++ search/functions_0.js
@@ -3,7 +3,7 @@ var searchData=
   ['active',['active',['../structdiy_1_1_regular_all_reduce_partners.html#ab697d800d6cbfe1520bebbccb057f18a',1,'diy::RegularAllReducePartners']]],
   ['add',['add',['../classdiy_1_1_master.html#aeab2723bb0ecd4f7316b4805c1b53a61',1,'diy::Master']]],
   ['all_5fgather',['all_gather',['../group___m_p_i.html#ga9346ef64e83013dc4a92b7b91e5ba79f',1,'diy::mpi::all_gather(const communicator &amp;comm, const T &amp;in, std::vector&lt; T &gt; &amp;out)'],['../group___m_p_i.html#ga0d1490d44460df450790269f7b1468cd',1,'diy::mpi::all_gather(const communicator &amp;comm, const std::vector&lt; T &gt; &amp;in, std::vector&lt; std::vector&lt; T &gt; &gt; &amp;out)']]],
-  ['all_5freduce',['all_reduce',['../structdiy_1_1_master_1_1_proxy.html#abce4381d1244875967dc3d127227bd45',1,'diy::Master::Proxy::all_reduce()'],['../group___m_p_i.html#ga44987b4d3a2fbc6b22f458617a445dc5',1,'diy::mpi::all_reduce()']]],
+  ['all_5freduce',['all_reduce',['../group___communication.html#gabce4381d1244875967dc3d127227bd45',1,'diy::Master::Proxy::all_reduce()'],['../group___m_p_i.html#ga44987b4d3a2fbc6b22f458617a445dc5',1,'diy::mpi::all_reduce()']]],
   ['all_5fto_5fall',['all_to_all',['../group___m_p_i.html#ga173b8718e3db0f8e804acb303b9db33b',1,'diy::mpi::all_to_all()'],['../group___communication.html#ga3dfb1bdb9c078d9f18fc6953295a0208',1,'diy::all_to_all()']]],
   ['assigner',['assigner',['../structdiy_1_1_reduce_proxy.html#ab3cba8511e114ecfd3e84a25f30f6bd9',1,'diy::ReduceProxy::assigner()'],['../classdiy_1_1_assigner.html#a0755324ae2441ad6eb0c2722fe389c84',1,'diy::Assigner::Assigner()']]]
 ];
