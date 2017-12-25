672507071aef92b956e664855280346acf935b26
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2017-12-24 22:42:01 -0500

    update tests for non-mpi config.

--- tests/scripts/simple.sh
+++ tests/scripts/simple.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-
 for np in 2 3; do
     let nb=${np}+1
-    ${1} -np ${np} ./simple-test -b ${nb}
+    if [ $# -eq 0 ]
+    then
+      ./simple-test -b ${nb}
+    else
+      ${1} -np ${np} ./simple-test -b ${nb}
+    fi
 done
