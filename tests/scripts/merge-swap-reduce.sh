83a6c35f44fd5599b0668fb11616b25705932f8f
Dmitriy Morozov <dmitriy@mrzv.org>
2018-01-02 10:32:06 -0800

    Fix bash problem in t/s/merge-swap-reduce.sh

--- tests/scripts/merge-swap-reduce.sh
+++ tests/scripts/merge-swap-reduce.sh
@@ -6,7 +6,7 @@ if [[ (`uname` == 'Darwin') && ($MPI == 'openmpi') ]]; then
     MAXP=2
 fi
 
-for p in {1..$MAXP}; do
+for p in $(seq 1 $MAXP); do
   for b in 2 4 8 9 12 24 36 44 48 56 64; do
       $1 -np $p ./merge-swap-reduce-test -b $b
   done
