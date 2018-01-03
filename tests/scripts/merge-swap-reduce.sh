aa778e24a40ec6d39c4f8b43eb4bdb4f2708219a
Dmitriy <dmitriy@mrzv.org>
2018-01-02 18:30:42 -0800

    Fix Travis (#50)

--- tests/scripts/merge-swap-reduce.sh
+++ tests/scripts/merge-swap-reduce.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-for p in 1 2 3; do
+MAXP=3
+
+if [[ (`uname` == 'Darwin') && ($MPI == 'openmpi') ]]; then
+    MAXP=2
+fi
+
+for p in $(seq 1 $MAXP); do
   for b in 2 4 8 9 12 24 36 44 48 56 64; do
       $1 -np $p ./merge-swap-reduce-test -b $b
   done
