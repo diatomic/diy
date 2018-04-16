a1be9d2a31524c6f2218fba8143bf1bfb55d85bc
Travis CI User <travis@example.org>
2018-04-16 16:34:52 +0000

    Update docs after building Travis build 91 of diatomic/diy
    The docs were built from the branch 'master' against the commit
    49d69fdc60811449cc9832945d01c6270c18fc1b.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/diatomic/diy/jobs/367217524.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.5.5/bin/doctr deploy --built-docs docs/html --gh-pages-docs .

--- search/all_4.js
+++ search/all_4.js
@@ -7,7 +7,7 @@ var searchData=
   ['enqueueiterator',['EnqueueIterator',['../structdiy_1_1_master_1_1_proxy_1_1_enqueue_iterator.html',1,'diy::Master::Proxy']]],
   ['environment',['environment',['../structdiy_1_1mpi_1_1environment.html',1,'diy::mpi']]],
   ['examples',['Examples',['../group___examples.html',1,'']]],
-  ['exchange',['exchange',['../classdiy_1_1_master.html#a211ba9c61e9d2d4f318a27316625989c',1,'diy::Master']]],
+  ['exchange',['exchange',['../classdiy_1_1_master.html#a832ae002e1031fa1b0c8956d6baf94e8',1,'diy::Master']]],
   ['externalstorage',['ExternalStorage',['../classdiy_1_1_external_storage.html',1,'diy']]],
   ['examples',['examples',['../md_docs_detail_examples.html',1,'']]]
 ];
--- search/all_7.js
+++ search/all_7.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['ibarrier',['ibarrier',['../classdiy_1_1mpi_1_1communicator.html#a86e998024eba15ce54bd2a3b0d2fa0a1',1,'diy::mpi::communicator']]],
   ['ibroadcast',['ibroadcast',['../group___m_p_i.html#gaba6ba09eb9dc9e3c2b024bf00dfc3d89',1,'diy::mpi']]],
   ['immediate_20mode',['Immediate Mode',['../group___immediate.html',1,'']]],
   ['in',['in',['../namespacediy.html#a861d8601719a75414c57d6aa6a99e0fc',1,'diy']]],
@@ -15,6 +16,7 @@ var searchData=
   ['iprobe',['iprobe',['../classdiy_1_1mpi_1_1communicator.html#a985ffab378f21b2b5f70a4cc1ddb3dc9',1,'diy::mpi::communicator']]],
   ['irecv',['irecv',['../classdiy_1_1mpi_1_1communicator.html#ac8953e70e3bb38f820279f2e3c57e9ff',1,'diy::mpi::communicator']]],
   ['isend',['isend',['../classdiy_1_1mpi_1_1communicator.html#ac30151886b5c0927314e051ccac2a69b',1,'diy::mpi::communicator']]],
+  ['issend',['issend',['../classdiy_1_1mpi_1_1communicator.html#a62fdce43a8d33fc367d20fb69fbfd213',1,'diy::mpi::communicator']]],
   ['immediate',['immediate',['../md_docs_detail_immediate.html',1,'']]],
   ['io',['io',['../md_docs_detail_io.html',1,'']]]
 ];
