2a4fc9fa3682af6611252825b7aa310de11bd8bc
Tom Peterka <tpeterka@mcs.anl.gov>
2018-11-05 16:27:29 -0600

    Continued cleaning up pick.hpp
    - Removed dimension argument from distance() and wrap_bounds()
    - Kept original verions for backward compatibility but deprecated them

--- include/diy/pick.hpp
+++ include/diy/pick.hpp
@@ -2,6 +2,7 @@
 #define DIY_PICK_HPP
 
 #include "link.hpp"
+#include "constants.h"      // for DEPRECATED
 
 namespace diy
 {
@@ -12,13 +13,26 @@ namespace diy
     void in(const RegularLink<Bounds>& link, const Point& p, OutIter out, const Bounds& domain);
 
     template<class Point, class Bounds, class Out = double>
+    Out distance(const Bounds& bounds, const Point& p);
+
+    template<class Point, class Bounds, class Out = double>
+    DEPRECATED("Use distance(const Bounds& bounds, const Point& p) instead.")
     Out distance(int dim, const Bounds& bounds, const Point& p);
 
     template<class Bounds, class Out = double>
     inline
+    Out distance(const Bounds& bounds1, const Bounds& bounds2);
+
+    template<class Bounds, class Out = double>
+    DEPRECATED("Use distance(const Bounds& bounds1, const Bounds& bounds2) instead.")
+    inline
     Out distance(int dim, const Bounds& bounds1, const Bounds& bounds2);
 
     template<class Bounds>
+    void wrap_bounds(Bounds& bounds, Direction wrap_dir, const Bounds& domain);
+
+    template<class Bounds>
+    DEPRECATED("Use wrap_bounds(Bounds& bounds, Direction wrap_dir, const Bounds& domain) instead.")
     void wrap_bounds(Bounds& bounds, Direction wrap_dir, const Bounds& domain, int dim);
 }
 
@@ -39,15 +53,39 @@ near(const RegularLink<Bounds>& link,  //!< neighbors
   {
     // wrap neighbor bounds, if necessary, otherwise bounds will be unchanged
     neigh_bounds = link.bounds(n);
-    wrap_bounds(neigh_bounds, link.wrap(n), domain, link.dimension());
+    wrap_bounds(neigh_bounds, link.wrap(n), domain);
 
-    if (distance(link.dimension(), neigh_bounds, p) <= r)
+    if (distance(neigh_bounds, p) <= r)
         *out++ = n;
   } // for all neighbors
 }
 
 //! Find the distance between point `p` and box `bounds`.
-template<class Point, class Bounds, class Out = double>
+template<class Point, class Bounds, class Out>
+Out
+diy::
+distance(const Bounds& bounds, const Point& p)
+{
+    double res = 0;
+    for (int i = 0; i < p.size(); ++i)
+    {
+        // avoids all the annoying case logic by finding
+        // diff = max(bounds.min[i] - p[i], 0, p[i] - bounds.max[i])
+        double diff = 0, d;
+
+        d = bounds.min[i] - p[i];
+        if (d > diff) diff = d;
+        d = p[i] - bounds.max[i];
+        if (d > diff) diff = d;
+
+        res += diff*diff;
+    }
+    return sqrt(res);
+}
+
+// DEPRECATED
+//! Find the distance between point `p` and box `bounds`.
+template<class Point, class Bounds, class Out>
 Out
 diy::
 distance(int dim, const Bounds& bounds, const Point& p)
@@ -69,7 +107,33 @@ distance(int dim, const Bounds& bounds, const Point& p)
     return sqrt(res);
 }
 
-template<class Bounds, class Out = double>
+template<class Bounds, class Out>
+Out
+diy::
+distance(const Bounds& bounds1, const Bounds& bounds2)
+{
+    double res = 0;
+    for (int i = 0; i < bounds1.min.size(); ++i)   // assume min, max of both bounds have same size
+    {
+        double diff = 0, d;
+
+        double d1 = bounds1.max[i] - bounds2.min[i];
+        double d2 = bounds2.max[i] - bounds1.min[i];
+
+        if (d1 > 0 && d2 > 0)
+            diff = 0;
+        else if (d1 <= 0)
+            diff = -d1;
+        else if (d2 <= 0)
+            diff = -d2;
+
+        res += diff*diff;
+    }
+    return sqrt(res);
+}
+
+// DEPRECATED
+template<class Bounds, class Out>
 Out
 diy::
 distance(int dim, const Bounds& bounds1, const Bounds& bounds2)
@@ -112,11 +176,27 @@ in(const RegularLink<Bounds>& link,  //!< neighbors
     neigh_bounds = link.bounds(n);
     wrap_bounds(neigh_bounds, link.wrap(n), domain, link.dimension());
 
-    if (distance(link.dimension(), neigh_bounds, p) == 0)
+    if (distance(neigh_bounds, p) == 0)
         *out++ = n;
   } // for all neighbors
 }
 
+// wraps block bounds
+// wrap dir is the wrapping direction from original block to wrapped neighbor block
+// overall domain bounds and dimensionality are also needed
+template<class Bounds>
+void
+diy::
+wrap_bounds(Bounds& bounds, Direction wrap_dir, const Bounds& domain)
+{
+  for (int i = 0; i < bounds.min.size(); ++i)      // assume min, max of bounds, domain have same size
+  {
+    bounds.min[i] += wrap_dir[i] * (domain.max[i] - domain.min[i]);
+    bounds.max[i] += wrap_dir[i] * (domain.max[i] - domain.min[i]);
+  }
+}
+
+// DEPRECATED
 // wraps block bounds
 // wrap dir is the wrapping direction from original block to wrapped neighbor block
 // overall domain bounds and dimensionality are also needed
@@ -131,6 +211,5 @@ wrap_bounds(Bounds& bounds, Direction wrap_dir, const Bounds& domain, int dim)
     bounds.max[i] += wrap_dir[i] * (domain.max[i] - domain.min[i]);
   }
 }
-
 
 #endif
