453aacd0918802f454a2839d805ff9e4b5c5f939
Robert Maynard <robert.maynard@kitware.com>
2019-05-20 10:56:13 -0400

    IExchangeInfo min_queue_size and max_hold_time are now size_t types.
    They are constructed from size_t values, and compared against
    size_t values, so they should be size_t.

--- include/diy/detail/master/iexchange.hpp
+++ include/diy/detail/master/iexchange.hpp
@@ -25,8 +25,7 @@ namespace diy
 
       // shortcut
       void              time_stamp_send()                       { time_last_send = Clock::now(); }
-      bool              hold(size_t queue_size)                 { return min_queue_size_ >= 0 && max_hold_time_ >= 0 &&
-                                                                         queue_size < min_queue_size_ && hold_time() < max_hold_time_; }
+      bool              hold(size_t queue_size)                 { return queue_size < min_queue_size_ && hold_time() < max_hold_time_; }
       size_t            hold_time()                             { return std::chrono::duration_cast<std::chrono::milliseconds>(Clock::now() - time_last_send).count(); }
       bool              fine() const                            { return fine_; }
 
@@ -38,8 +37,8 @@ namespace diy
       std::shared_ptr<spd::logger>        log = get_logger();
       Time                                time_last_send;       // time of last send from any queue in send_outgoing_queues()
 
-      int                                 min_queue_size_;      // minimum short message size (bytes)
-      int                                 max_hold_time_;       // maximum short message hold time (milliseconds)
+      size_t                              min_queue_size_;      // minimum short message size (bytes)
+      size_t                              max_hold_time_;       // maximum short message hold time (milliseconds)
 
       int                                 from_gid = -1;        // gid of current block, for shortcut sending of only this block's queues
 
