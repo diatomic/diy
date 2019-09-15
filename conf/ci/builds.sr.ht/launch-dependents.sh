bf7d7252f56e288ca3d7a0ba93991ffea0a9bf04
Dmitriy Morozov <dmitriy@mrzv.org>
2019-09-15 14:16:57 -0700

    Add .build.yml to test dependents on builds.sr.ht

---
+++ conf/ci/builds.sr.ht/launch-dependents.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+TOKEN=`cat ~/.sr.ht.token`
+AUTH=Authorization:"token ${TOKEN}"
+
+REV=`git rev-parse HEAD`
+REV_SHORT=`git rev-parse --short HEAD`
+
+curl https://raw.githubusercontent.com/diatomic/tess2/master/.build.yml |
+(cat && echo "environment: { DIY_REV: $REV }") |
+jq -sR '{"manifest": .,"note":"Test tess2 with DIY rev ['$REV_SHORT'](https://github.com/diatomic/diy/commit/'$REV')"}' |
+curl --verbose \
+    -H "$AUTH" \
+    -H "Content-Type: application/json" \
+    -X POST \
+    -d @- \
+    https://builds.sr.ht/api/jobs
