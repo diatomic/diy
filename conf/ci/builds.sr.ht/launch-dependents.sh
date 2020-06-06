13d3e67c1bd027a45cfc1315884a3ae58824ef93
Dmitriy Morozov <dmitriy@mrzv.org>
2020-06-05 19:21:38 -0700

    Add Reeber2 to dependents testing

--- conf/ci/builds.sr.ht/launch-dependents.sh
+++ conf/ci/builds.sr.ht/launch-dependents.sh
@@ -15,3 +15,15 @@ curl --silent \
     -X POST \
     -d @- \
     https://builds.sr.ht/api/jobs
+
+GH_TOKEN=`cat ~/.github.token`
+GH_AUTH=Authorization: token $GH_TOKEN
+curl -H "$GH_AUTH" https://raw.githubusercontent.com/mrzv/Reeber2/master/.build.yml |
+(cat && echo "environment: { DIY_REV: $REV }") |
+jq -sR '{"manifest": .,"note":"Test Reeber2 with DIY rev ['$REV_SHORT'](https://github.com/diatomic/diy/commit/'$REV')"}' |
+curl --silent \
+    -H "$AUTH" \
+    -H "Content-Type: application/json" \
+    -X POST \
+    -d @- \
+    https://builds.sr.ht/api/jobs
