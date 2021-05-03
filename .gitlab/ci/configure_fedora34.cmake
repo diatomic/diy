1c42c9f4ab174d208a44c031877c2f53eef141a1
Ben Boeckel <ben.boeckel@kitware.com>
2021-05-03 16:18:45 -0400

    ci: disable avx512 on CI
    Not all machines support avx512 instructions.

--- .gitlab/ci/configure_fedora34.cmake
+++ .gitlab/ci/configure_fedora34.cmake
@@ -1,1 +1,7 @@
+# Build binaries that will run on older architectures
+if ("$ENV{CMAKE_CONFIGURATION}" MATCHES "fedora")
+  set(CMAKE_C_FLAGS "-march=core2 -mno-avx512f" CACHE STRING "")
+  set(CMAKE_CXX_FLAGS "-march=core2 -mno-avx512f" CACHE STRING "")
+endif ()
+
 include("${CMAKE_CURRENT_LIST_DIR}/configure_common.cmake")
