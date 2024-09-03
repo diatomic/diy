diatomic/diy

commit 12e3ce8c1255fdaff80d7a611fdb761b2e8be9b1

Fix up master.release()

include/diy/collection.hpp
@@ -40,7 +40,7 @@ namespace diy
       inline void   clear();
 
       int           add(Element e)                  { elements_.push_back(e); external_.push_back(-1); ++(*in_memory_.access()); return static_cast<int>(elements_.size()) - 1; }
-      void*         release(int i)                  { void* e = get(i); elements_[static_cast<size_t>(i)] = 0; return e; }
+      void*         release(int i);
 
       void*         find(int i) const               { return elements_[static_cast<size_t>(i)]; }                        // possibly returns 0, if the element is unloaded
       void*         get(int i)                      { if (!find(i)) load(i); return find(i); }      // loads the element first, and then returns its address
@@ -85,6 +85,23 @@ clear()
   elements_.clear();
   external_.clear();
   *in_memory_.access() = 0;
+}
+
+void*
+diy::Collection::
+release(int i)
+{
+  void* e = get(i);
+
+  elements_[static_cast<size_t>(i)] = 0;
+  std::swap(elements_[static_cast<size_t>(i)], elements_.back());
+  elements_.pop_back();
+
+  std::swap(external_[static_cast<size_t>(i)], external_.back());
+  external_.pop_back();
+  --(*in_memory_.access());
+
+  return e;
 }
 
 void

include/diy/master.hpp
@@ -591,7 +591,11 @@ diy::Master::
 release(int i)
 {
   void* b = blocks_.release(i);
+
   delete link(i);   links_[i] = 0;
+  std::swap(links_[i], links_.back());
+  links_.pop_back();
+
   lids_.erase(gid(i));
   return b;
 }

commit a6d87f60f3f5b322fa603a01550ebba6e7ea9827

mark Collection::release as inline
If also `release` is marked as inline, just as all the other methods
defined outside the class definition of `Collection`, then the header
can be included from several compilation units without causing multiple
definitions of this method. This was broken in
12e3ce8c1255fdaff80d7a611fdb761b2e8be9b1.

include/diy/collection.hpp
@@ -40,7 +40,7 @@ namespace diy
       inline void   clear();
 
       int           add(Element e)                  { elements_.push_back(e); external_.push_back(-1); ++(*in_memory_.access()); return static_cast<int>(elements_.size()) - 1; }
-      void*         release(int i);
+      inline void*  release(int i);
 
       void*         find(int i) const               { return elements_[static_cast<size_t>(i)]; }                        // possibly returns 0, if the element is unloaded
       void*         get(int i)                      { if (!find(i)) load(i); return find(i); }      // loads the element first, and then returns its address